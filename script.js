25941b76f61772022f52acdfc0fbb1cf843b8bd9
AASSURYA <akkireddyanilsaisurya888@gmail.com>
2025-10-22 22:51:02 +0530

    Birthday wishes to Anil

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const correctPassword = "Geetha@143";
+const correctPassword = "Anil@143";
 
 function checkPassword() {
     const userInput = document.getElementById('passwordInput').value;
@@ -13,7 +13,7 @@ function checkPassword() {
 }
 
 function showMessage() {
-    const message = "🎉 Surprise! 🎉 Geetha! I know your birthday isn’t for a few days, but I just had to send you an early surprise. 🎉Wishing you an amazing year ahead filled with happiness, success, and all the things you love. Consider this an early start to the celebrations—happy advance birthday! 🎉🎂";
+    const message = "🎉 Surprise! 🎉 Anil! I know your birthday isn't for a few days, but I just had to send you an early surprise. 🎉Wishing you an amazing year ahead filled with happiness, success, and all the things you love. Consider this an early start to the celebrations—happy advance birthday! 🎉🎂";
     document.getElementById('surpriseMessage').textContent = message;
     document.getElementById('surpriseMessage').style.display = 'block';
 }
